d4f8c9966cb2bc8ce595e8bb402c7fe8c4656eee
Grinevich Artem <grinevitch.artemy@yandex.ru>
2023-02-06 17:00:59 +0300

    подготовить шаблон проекта

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import Drawer from './components/Drawer';
-import Header from './components/Header';
-import Card from './components/Card';
+import React from 'react';
+// import axios from 'axios';
 
 function App() {
   return (
     <div className="wrapper">
-      <Drawer />
-      <Header />
       <div className="content">
-        <div className="titl">
-          <h1>Все кроссовки</h1>
-          <div className="search-block">
-            <img src="/img/search.svg" alt="search" />
-            <input placeholder="Поиск..." />
-          </div>
-        </div>
-        <div className="cards">
-          <Card />
-        </div>
+       
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+//import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
+
+window.React1 = require('react');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>
+  
 );
