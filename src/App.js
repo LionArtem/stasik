f1f2b159422196f8c4d73182eea2708f9a4a14d1
Grinevich Artem <grinevitch.artemy@yandex.ru>
2023-02-06 22:08:56 +0300

    сделать поиск

--- src/App.js
+++ src/App.js
@@ -4,19 +4,58 @@ import React from 'react';
 function App() {
   const token = 'sO7zGyswnmlJSN5XZ342f_OX4cKzRMA1Pg02vMAny-s';
 
-  React.useEffect(() => {
-    fetch('https://api.unsplash.com/photos/random', {
+  let searchVelue
+
+  const [items, setItems] = React.useState([]);
+
+  // const form = document.querySelector('.search__form');
+  // console.log(form);
+  // form.addEventListener('submit',(evt)=>{
+  //   evt.preventDefault()
+  //   searchSabmit()
+  // })
+
+  const searchSabmit = (searchVelue) => {
+    fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchVelue}`, {
       headers: {
         authorization: `Client-ID ${token}`,
         'Content-Type': 'application/json',
       },
     })
       .then((res) => {
-        return res.json();
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Ошибка: ${res.status}`);
       })
-      .then((json) => {
-        console.log(json);
-        // setItems(json);
+      .then((res) => {
+        console.log(res);
+        setItems(res.results);
+      })
+      .catch((res) => {
+        console.log(res);
+      });
+  };
+
+  React.useEffect(() => {
+    fetch('https://api.unsplash.com/photos/random?&count=10', {
+      headers: {
+        authorization: `Client-ID ${token}`,
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Ошибка: ${res.status}`);
+      })
+      .then((res) => {
+        console.log(res);
+        setItems(res);
+      })
+      .catch((res) => {
+        console.log(res);
       });
   }, []);
 
@@ -27,11 +66,24 @@ function App() {
       </header>
       <main>
         <section>
-          <form>
-            <input></input>
+          <form className="search__form">
+            <input value={searchVelue}></input>
+            <button
+              className="search__botton"
+              type="submit"
+              onClick={()=> searchSabmit(searchVelue)}
+            >
+              найти
+            </button>
           </form>
         </section>
-        <section></section>
+        <section>
+          {items.map((item) => (
+            <>
+              <img width={200} height={200} src={item.urls.small} />
+            </>
+          ))}
+        </section>
         <footer>Stasik</footer>
       </main>
     </div>
