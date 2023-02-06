bfe5060ea38367e75d2f0b33f5b8c266d07f14a0
Grinevich Artem <grinevitch.artemy@yandex.ru>
2023-02-06 18:33:08 +0300

    получить по запросц фото

--- src/App.js
+++ src/App.js
@@ -2,11 +2,38 @@ import React from 'react';
 // import axios from 'axios';
 
 function App() {
+  const token = 'sO7zGyswnmlJSN5XZ342f_OX4cKzRMA1Pg02vMAny-s';
+
+  React.useEffect(() => {
+    fetch('https://api.unsplash.com/photos/random', {
+      headers: {
+        authorization: `Client-ID ${token}`,
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        console.log(json);
+        // setItems(json);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
-      <div className="content">
-       
-      </div>
+      <header>
+        <h1>Поиск фотографий</h1>
+      </header>
+      <main>
+        <section>
+          <form>
+            <input></input>
+          </form>
+        </section>
+        <section></section>
+        <footer>Stasik</footer>
+      </main>
     </div>
   );
 }
